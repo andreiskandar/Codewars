56e4ee9e25b0ea28ab1480b54c2a9b75ef08c549
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-08-06 20:25:54 -0700

    add katas

---
+++ 7kyu/7kyu-arrayPrototypeReverse.js
@@ -0,0 +1,19 @@
+// 7 kyu - Array.prototype.reverse()
+
+// The Array's reverse() method has gone missing! Re-write it, quick-sharp!
+
+// When this method is called, it reverses the order of the items in the original array. Then then it returns that same, original array. No new arrays should need to be created to pass this kata.
+
+// Here's an example:
+
+// var input = [1, 2, 3, 4];
+// input.reverse(); // == [4, 3, 2, 1]  // returned by .reverse()
+// input;           // == [4, 3, 2, 1]  // items reordered in the original array
+
+Array.prototype.reverse = function () {
+	const reversed = this.reduceRight((acc, curr) => [...acc, curr], []);
+	Object.assign(this, reversed);
+	return this;
+};
+
+console.log([1, 2, 4, 5, 6].reverse());
---
+++ 8kyu/8kyu-grasshoopper-TerminalGame1.js
@@ -0,0 +1,19 @@
+// 8 kyu - Grasshopper - Terminal Game #1
+
+// Terminal Game - Create Hero Prototype
+// In this first kata in the series, you need to define a Hero prototype to be used in a terminal game. The hero should have the following attributes:
+
+// attribute	value
+// name	user argument or 'Hero'
+// position	'00'
+// health	100
+// damage	5
+// experience	0
+
+function Hero(name) {
+	this.name = name || 'Hero';
+	this.position = '00';
+	this.health = 100;
+	this.damage = 0;
+	this.experience = 0;
+}
